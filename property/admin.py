1c0ca4e1f1ceb6fa8b94f5749603c3040d1d9292
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-08-30 13:37:35 +0300

    add search to admin Flat

--- property/admin.py
+++ property/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from .models import Flat
 
 
-admin.site.register(Flat)
+class AdminFlat(admin.ModelAdmin):
+    search_fields = (
+        'town',
+        'town_district',
+        'address',
+    )
+
+
+admin.site.register(Flat, AdminFlat)
--- property/models.py
+++ property/models.py
@@ -3,49 +3,71 @@ from django.utils import timezone
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner = models.CharField(
+        verbose_name='ФИО владельца',
+        max_length=200,
+    )
+    owners_phonenumber = models.CharField(
+        verbose_name='Номер владельца',
+        max_length=20,
+    )
     created_at = models.DateTimeField(
-        'Когда создано объявление',
+        verbose_name='Когда создано объявление',
         default=timezone.now,
-        db_index=True)
-
-    description = models.TextField('Текст объявления', blank=True)
-    price = models.IntegerField('Цена квартиры', db_index=True)
-
+        db_index=True,
+    )
+    description = models.TextField(
+        verbose_name='Текст объявления',
+        blank=True,
+    )
+    price = models.IntegerField(
+        verbose_name='Цена квартиры',
+        db_index=True,
+    )
     town = models.CharField(
-        'Город, где находится квартира',
+        verbose_name='Город, где находится квартира',
         max_length=50,
-        db_index=True)
+        db_index=True,
+    )
     town_district = models.CharField(
-        'Район города, где находится квартира',
+        verbose_name='Район города, где находится квартира',
         max_length=50,
         blank=True,
-        help_text='Чертаново Южное')
+        help_text='Чертаново Южное',
+    )
     address = models.TextField(
-        'Адрес квартиры',
-        help_text='ул. Подольских курсантов д.5 кв.4')
+        verbose_name='Адрес квартиры',
+        help_text='ул. Подольских курсантов д.5 кв.4',
+    )
     floor = models.CharField(
-        'Этаж',
+        verbose_name='Этаж',
         max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
-
+        help_text='Первый этаж, последний этаж, пятый этаж',
+    )
     rooms_number = models.IntegerField(
-        'Количество комнат в квартире',
-        db_index=True)
+        verbose_name='Количество комнат в квартире',
+        db_index=True,
+    )
     living_area = models.IntegerField(
-        'количество жилых кв.метров',
+        verbose_name='количество жилых кв.метров',
         null=True,
         blank=True,
-        db_index=True)
-
-    has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
-    active = models.BooleanField('Активно-ли объявление', db_index=True)
+        db_index=True,
+    )
+    has_balcony = models.NullBooleanField(
+        verbose_name='Наличие балкона',
+        db_index=True,
+    )
+    active = models.BooleanField(
+        verbose_name='Активно-ли объявление',
+        db_index=True,
+    )
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
         blank=True,
-        db_index=True)
+        db_index=True,
+    )
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
