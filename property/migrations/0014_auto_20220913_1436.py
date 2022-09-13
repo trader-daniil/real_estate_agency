bbd64da3aedd660e4660a8a961f3790973579be9
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-09-13 21:11:12 +0300

    bound owners and flats

---
+++ property/migrations/0014_auto_20220913_1436.py
@@ -0,0 +1,36 @@
+from django.db import migrations
+
+def add_flats_owners_relations(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    Owner = apps.get_model('property', 'Owner')
+    for flat in Flat.objects.all():
+        owner = Owner.objects.filter(flats__in=[flat]).all().first()
+        if not owner:
+            owner = Owner.objects.create(
+                full_name=flat.owner,
+                owners_phonenumber=flat.owners_phonenumber,
+                owner_pure_phone=flat.owner_pure_phone
+            )
+        owner.flats.add(flat)
+        owner.save()
+
+
+def move_backward(apps, schema_editor):
+    Owner = apps.get_model('property', 'Owner')
+    for owner in Owner.objects.all():
+        owner.flats.clear()
+        owner.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('property', '0013_auto_20220913_1425'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            add_flats_owners_relations,
+            move_backward,
+        ),
+    ]
